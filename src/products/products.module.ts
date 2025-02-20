6ced4bef7f68a809cc3e6b65b7b5bf2b3e15c468
ChanHengLucas <91720922+ChanHengLucas@users.noreply.github.com>
2025-02-20 11:57:22 -0500

    some progression i suppose

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { ProductSchema } from './product.model';
 
 @Module({
     imports: [
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -12,15 +12,25 @@ export class ProductsService {
         @InjectModel('Product') private readonly productModel: Model<Product>
     ) {}
 
-    insertProduct(title: string, desc: string, price: number) {
-        const prodId = Math.random().toString();
-        const newProduct = new this.productModel(prodId, title, desc, price);
-        this.products.push(newProduct);
-        return prodId;
+    async insertProduct(title: string, desc: string, price: number) {
+        const newProduct = new this.productModel({
+            title, 
+            desc, 
+            price,
+        });
+        const result = await newProduct.save(); // waits for data to save to database
+        console.log(result);
+        return result.id as string;
     }
 
-    getProducts() {
-        return [...this.products];
+    async getProducts() {
+        const products = await this.productModel.find().exec(); // returns real promise
+        return products.map((prod) => ({
+            id: prod.id, 
+            title: prod.title, 
+            desc: prod.desc, 
+            price: prod.price
+        })); // yields a list of products
     }
 
     getSingleProduct(productId: string) {
